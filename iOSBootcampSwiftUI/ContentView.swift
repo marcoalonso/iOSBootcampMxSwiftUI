82e164dc9ee4e47f974c9fccb4d4c3ba85b101d7
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-03-19 07:13:09 -0600

    Empezando

---
+++ iOSBootcampSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iOSBootcampSwiftUI
+//
+//  Created by Marco Alonso Rodriguez on 19/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOSBootcampSwiftUI/iOSBootcampSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSBootcampSwiftUIApp.swift
+//  iOSBootcampSwiftUI
+//
+//  Created by Marco Alonso Rodriguez on 19/03/23.
+//
+
+import SwiftUI
+
+@main
+struct iOSBootcampSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
