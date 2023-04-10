ed7c91f03fa56fdc722529b5c20c9d98fdd5c513
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-04-10 14:04:17 -0600

    Main SearchBar Added

--- iOSBootcampSwiftUI/ContentView.swift
+++ iOSBootcampSwiftUI/ContentView.swift
@@ -9,19 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     
-    typealias Options = [(AnyView, String)]
     typealias ExampleViews = [(AnyView, String, String)]
     
-    var navigation: ExampleViews = [
+    @State private var searchterm: String = ""
+    
+    var options: ExampleViews = [
         (AnyView(NavigationButtonClick()), "Navigation Simple", "Navigation based on Button click"),
         (AnyView(PassDataFromList()), "Passing Data from List", "how to pass data to a detail view"),
         (AnyView(ModalSheet()), "Sheet with Modal", "Show a view with environment dismiss"),
         (AnyView(NavigationProgrammatically()), "NavigationProgrammatically", "Navigate other view after 2 seconds"),
         (AnyView(PassingData()), "Passing Data", "Different types of data"),
-        (AnyView(DownloadWithCombine()), "Download with combine", "From fake API")
-    ]
-    
-    var lists: ExampleViews = [
+        (AnyView(DownloadWithCombine()), "Download with combine", "From fake API"),
         (AnyView(PullToRefreshDemo()), "Pull To Refresh Demo", "Implementing a refreshable"),
         (AnyView(DisplayItemsInList()), "Display Items In List", "Numbers, Custom Objects Indices"),
         (AnyView(FormDemo()), "Form Demo", "Simple form with alert and func to validate"),
@@ -30,53 +28,37 @@ struct ContentView: View {
         (AnyView(AddingMovingRemovingItems()), "Adding Moving And Removing", "Items in the list"),
         (AnyView(SearchItemsList()), "Search items in list", "With searchable and onChange"),
         (AnyView(AutoScrollListDemo()), "Auto Scroll List", "ScrollView Reader"),
+        (AnyView(Images()), "Images",""),
+        (AnyView(PinnedViews()), "PinnedViews",""),
+        (AnyView(Grids()), "Grids",""),
+        (AnyView(TextDemo()), "Texts",""),
+        (AnyView(ScrollViewDemo()), "ScrollView",""),
+        (AnyView(DatePickerDemo()), "Date Picker Demo",""),
+        (AnyView(ObservedObjectDemo()), "Observed Object",""),
+        (AnyView(Colors()), "Colors",""),
+        (AnyView(Buttons()), "Buttons",""),
+        (AnyView(HeroAnimations()), "Animation Geometry",""),
+        (AnyView(NamespaceDemo()), "NamespaceDemo",""),
+        (AnyView(Shapes()), "Shapes",""),
+        (AnyView(Animation()), "Animation",""),
+        (AnyView(Icons()), "Icons",""),
+        (AnyView(Icons()), "Backgroud/Overlays",""),
+        (AnyView(MagnifyView()), "MagnifyView",""),
+        (AnyView(RotateView()), "Rotation View",""),
+        (AnyView(DragView()), "Drag View",""),
+        (AnyView(RotateAnimation3D()), "Rotation 3D View",""),
         (AnyView(SearchItemsList()), "Title", "subtitle"),
-        
-        
-        
-        
     ]
-    
-    var general: Options = [
-        (AnyView(Images()), "Images"),
-        (AnyView(PinnedViews()), "PinnedViews"),
-        (AnyView(Grids()), "Grids"),
-        (AnyView(TextDemo()), "Texts"),
-        (AnyView(ScrollViewDemo()), "ScrollView"),
-        (AnyView(DatePickerDemo()), "Date Picker Demo"),
-        (AnyView(ObservedObjectDemo()), "Observed Object"),
-        (AnyView(Colors()), "Colors"),
-        (AnyView(Buttons()), "Buttons"),
-        (AnyView(HeroAnimations()), "Animation Geometry"),
-        (AnyView(NamespaceDemo()), "NamespaceDemo")
-    ]
-    
-    var shapes: Options = [
-    (AnyView(Shapes()), "Shapes"),
-    (AnyView(Animation()), "Animation"),
-    (AnyView(Icons()), "Icons"),
-    (AnyView(Icons()), "Backgroud/Overlays"),
-    ]
-    
-    
-    
-    
-    var gestures: Options = [
-        (AnyView(MagnifyView()), "MagnifyView"),
-        (AnyView(RotateView()), "Rotation View"),
-        (AnyView(DragView()), "Drag View"),
-        (AnyView(RotateAnimation3D()), "Rotation 3D View"),
-    ]
-   
-    
     
     var body: some View {
-    
         NavigationView {
-            List {
-                // MARK:  Navigation
-                Section(header: Text("Navigation")) {
-                    ForEach(navigation, id: \.1) { current in
+            ScrollViewReader { proxy in
+                List {
+                    SearchBar(searchTerm: $searchterm)
+                        .listRowInsets(EdgeInsets(top: .zero, leading: .zero, bottom: .zero, trailing: .zero))
+                        .listRowSeparator(.hidden)
+                    
+                    ForEach(filteredOptions(), id: \.1) { current in
                         
                         NavigationLink(destination: current.0
                                        , label: {
@@ -88,84 +70,61 @@ struct ContentView: View {
                             }
                         })
                     }
+                    .id("Content")
                 }
-                
-                // MARK:  Navigation
-                Section(header: Text("Lists")) {
-                    ForEach(lists, id: \.1) { current in
-                        
-                        NavigationLink(destination: current.0
-                                       , label: {
-                            VStack(alignment: .leading) {
-                                Text(current.1)
-                                    .font(.headline)
-                                Text(current.2)
-                                    .font(.subheadline)
-                            }
-                        })
-                    }
+                .onAppear {
+                    proxy.scrollTo("Content", anchor: .top)
                 }
+                .animation(.linear, value: searchterm)
+                .listStyle(.plain)
+            }
+            .navigationTitle("iOS Bootcamp SwiftUI")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+    
+    func filteredOptions() -> ExampleViews {
+        options.filter { (_, name, description) in
+                guard !searchterm.isEmpty else { return true }
+                return (name + description)
+                .lowercased()
+                .contains(searchterm.lowercased())
+            }
+        .sorted(by: { $0.1 < $1.1 })
+        }
+}
+
+struct SearchBar: View {
+    @Binding var searchTerm: String
+    let backgroundColor : Color = Color.secondary
+    
+    var body: some View {
+        VStack(spacing: .zero) {
+            ZStack {
+                TextField("Filtrar", text: $searchTerm)
+                    .disableAutocorrection(true)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
                 
-                // MARK:  Gestures
-                Section(header: Text("Gestures")) {
-                    ForEach(gestures, id: \.1) { current in
-                        
-                        NavigationLink(destination: current.0
-                                       , label: {
-                            VStack(alignment: .leading) {
-                                Text(current.1)
-                                    .font(.headline)
-                                
-                            }
-                        })
-                    }
-                }
-                
-                // MARK:  - Gestures
-                Section(header: Text("Gestures")) {
-                    ForEach(gestures, id: \.1) { gesture in
-                        NavigationLink (destination: gesture.0) {
-                            VStack(alignment: .leading) {
-                                Text(gesture.1)
-                                    .font(.headline)
-                            }
+                //Clear Button
+                HStack {
+                    Spacer()
+                    Button {
+                        withAnimation {
+                            searchTerm.removeAll()
                         }
-                    }
-                }
-                
-                // MARK:  Shapes
-                Section(header: Text("Shapes")) {
-                    ForEach(shapes, id: \.1) { current in
-                        
-                        NavigationLink(destination: current.0
-                                       , label: {
-                            VStack(alignment: .leading) {
-                                Text(current.1)
-                                    .font(.headline)
-                            }
-                        })
-                    }
-                }
-                
-                // MARK:  General
-                Section(header: Text("General")) {
-                    ForEach(general, id: \.1) { current in
-                        
-                        NavigationLink(destination: current.0
-                                       , label: {
-                            VStack(alignment: .leading) {
-                                Text(current.1)
-                                    .font(.headline)
-                            }
-                        })
+                    } label: {
+                        Image(systemName: "xmark.circle.fill")
+                            .foregroundColor(.gray)
+                            .padding()
                     }
                     
+                    .frame(width: 50, height: 50)
                 }
-                
-            }//List
-            .listStyle(GroupedListStyle())
-            .navigationTitle("iOS Bootcamp SwiftUI")
+            }
+            .padding()
+            .background(backgroundColor)
         }
+        .id("Searchbar")
     }
 }
 
