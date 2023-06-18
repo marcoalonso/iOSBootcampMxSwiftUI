0dfa87f276a32e93a578eef265f5242f36f771e1
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-06-17 23:22:43 -0600

    Webpage modified and only one NavigationView in WebView

--- iOSBootcampSwiftUI/General/WebViewDemo.swift
+++ iOSBootcampSwiftUI/General/WebViewDemo.swift
@@ -10,38 +10,37 @@ import WebKit
 
 struct WebViewDemo: View {
     var body: some View {
-        NavigationView {
-            VStack {
-                Text("Option 1, with maxHeight 400 ")
-                    .font(.title3)
-                    .foregroundColor(.white)
-                WebView(urlString: "https://apple.com")
-                    .frame(maxHeight: 400)
-                
-                Text("Option 2")
-                    .font(.title3)
-                    .foregroundColor(.blue)
-                
-                NavigationLink(destination: WebView(urlString: "https://apple.com")) {
-                    HStack {
-                        Image(systemName: "arrow.right")
-                            .bold()
-                        Text("Apple.com")
-                            .font(.title)
-                        Image(systemName: "globe")
-                    }
-                    .foregroundColor(.white)
-                    .padding(5)
-                    .background(Color.red)
-                    .cornerRadius(12)
+        VStack {
+            Text("Option 1, with maxHeight 400 ")
+                .font(.title3)
+                .foregroundColor(.white)
+            WebView(urlString: "https://github.com/marcoalonso")
+                .frame(maxHeight: 400)
+            
+            Text("Option 2")
+                .font(.title3)
+                .foregroundColor(.blue)
+            
+            NavigationLink(destination: WebView(urlString: "https://apple.com")) {
+                HStack {
+                    Image(systemName: "arrow.right")
+                        .bold()
+                    Text("Apple.com")
+                        .font(.title)
+                    Image(systemName: "globe")
                 }
-                
-                Spacer()
+                .foregroundColor(.white)
+                .padding(5)
+                .background(Color.red)
+                .cornerRadius(12)
             }
-            .background(Color.black)
-            .padding(.top, 10)
-            .navigationBarTitle("Web View ", displayMode: .inline)
+            
+            Spacer()
         }
+        .background(Color.black)
+        .padding(.top, 10)
+        .navigationBarTitle("Web View ", displayMode: .inline)
+        
     }
 }
 
